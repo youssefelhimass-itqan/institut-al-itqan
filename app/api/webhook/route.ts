703c2964419ee806d64e6890149f4a614d6eda4e
youssefelhimass-itqan <youssef.elhimass@gmail.com>
2026-06-03 16:54:23 +0200

    Correction Stripe

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -20,7 +20,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+   
 })
 
 // Stripe exige le corps brut (non parsé) pour valider la signature
