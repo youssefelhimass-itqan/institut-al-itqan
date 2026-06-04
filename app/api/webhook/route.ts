01a7b95cf2bba9a77b5fa1d9b0300cd64eaf5a25
youssefelhimass-itqan <youssef.elhimass@gmail.com>
2026-06-04 02:20:02 +0200

    Correction Stripe API version

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -16,7 +16,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  
 })
 
 const FORMULE_LABEL: Record<string, string> = {
