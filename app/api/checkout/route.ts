6467324cee012e7ec16d36956a9ea7f5d1bec693
youssefelhimass-itqan <youssef.elhimass@gmail.com>
2026-06-07 22:31:27 +0200

    Fix Stripe API version

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -7,7 +7,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  
 })
 
 const PRIX_FOIS4: Record<string, Record<number, string>> = {
